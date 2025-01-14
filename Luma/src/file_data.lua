85fbcadc9f016002584a96c35f408f4050233f53
Lix <lixinrong4002@gmail.com>
2025-01-14 22:17:11 +0800

    update file_data

--- Luma/src/file_data.lua
+++ Luma/src/file_data.lua
@@ -2,10 +2,11 @@ local file_data_module = {}
 
 file_data_module.lfs = require("lfs")
 
-file_data_module.file_data = {
-	["file_type"] = "", -- 文件类型，例如 "text", "image", "video", "directory" 等
+local file_data = {
 	["file_name"] = "", -- 文件名，带扩展名
 	["file_extension"] = "", -- 文件扩展名，例如 "txt", "jpg", "mp4" 等
+	["mode"] = false, -- 是否为目录
+	["file_type"] = "", -- 文件类型，例如 "text", "image", "video", "directory" 等
 	["file_size"] = 0, -- 文件大小（以字节为单位）
 	["file_size_readable"] = "", -- 可读的文件大小，例如 "1.2 KB", "3 MB"
 	["file_last_modified"] = "", -- 文件最后修改时间（格式化字符串）
@@ -24,14 +25,55 @@ file_data_module.file_data = {
 	["file_tags"] = {}, -- 文件标签（用户自定义的一些分类标记）
 }
 
+--- get the file extension name 
+--- @param abs_file_path string the absolute file path
+--- @retrun string | nil the extension name
+function file_data_module:get_extension_name(abs_file_path)
+  local extension = abs_file_path:match("^.+%.(.+)$")
+  return extension
+end
+
+print(file_data_module:get_extension_name("/home/orangepi/test.txt"))
+print(file_data_module:get_extension_name("/home/orangepi/test"))
+
+function file_data_module:telling_file_type()
+
+end
+
+
 function file_data_module:new(file_abs_path)
+	-- check the argument
 	if file_abs_path == nil or type(file_abs_path) ~= "string" then
 		error("file_abs_path is required and must be a string")
 	end
-	local data = self.lfs.attribute(file_abs_path)
+
+	local data = self.lfs.attributes(file_abs_path)
+	-- check whether the file has been exists
 	if data == nil then
 		error("file is not exists")
 	end
+
+	-- create a new obj in order to retrun them
+	local obj = {}
+
+  -- the file name
+  obj.file_name = data.
+
+  -- the file extension
+  obj.file_extension = data.extension
+
+	-- get the file datas
+	obj.file_data = file_data
+	-- mode
+	obj.data.mode = data.mode
+  -- type 
+  obj.data.file_type = 
+
+	return obj
 end
+
+function file_data_module:stirb() end
+
+obj = file_data_module:new("/home/orangepi/test.txt")
 
 return file_data_module
